de54657373145a89ddce2ea9789eb18e972b9ea5
Halfpint <alexander.sims92@gmail.com>
2021-10-15 08:18:10 +0100

    Parse date to correct postgres format

--- src/commands/raids/add.ts
+++ src/commands/raids/add.ts
@@ -3,7 +3,7 @@ import { getConnection } from "../../util/db";
 export default async (
   raidName: string
 ): Promise<{ id: number; name: string; date: string }> => {
-  const date = new Date().toLocaleDateString();
+  const date = getFormattedDate(new Date());
 
   const raids = await getConnection()
     .insert({
@@ -18,3 +18,15 @@ export default async (
 
   return { id: raids[0]?.id, name: `${raidName}@${date}`, date };
 };
+
+function getFormattedDate(date: any) {
+  var year = date.getFullYear();
+
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : "0" + month;
+
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
+
+  return month + "/" + day + "/" + year;
+}
