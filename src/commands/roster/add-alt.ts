5cc83b244f8c7bcb61079dc391fd9d849861c26a
Alexander Sims <alexander.sims92@gmail.com>
2022-03-01 19:20:07 +0000

    fixed bug with adding alts

--- src/commands/roster/add-alt.ts
+++ src/commands/roster/add-alt.ts
@@ -10,20 +10,13 @@ export default async (
   log.info({ main_id: player_id, box_ids: alt_ids });
   try {
     await knex.transaction(async (trx) => {
-      const delegatedBoxes = await getDelegatedAlts(player_id);
-      const alts_to_apply = new Set([
-        ...delegatedBoxes.map((box) => box.id),
-        ...alt_ids,
-      ]);
-
-      log.debug([player_id, ...alts_to_apply]);
       const playersToAdd = (
         await trx()
           .select('id')
           .from('player')
           .whereIn(
             'id',
-            [player_id, ...alts_to_apply].map((id: any) => `${id}`)
+            [player_id, ...alt_ids].map((id: any) => `${id}`)
           )
       ).map((player) => `${player.id}`);
 
@@ -33,11 +26,10 @@ export default async (
         );
       }
 
-      log.info(playersToAdd);
       const res = await trx()
         .from('player_alt')
-        .whereIn('player_id', playersToAdd)
-        .orWhereIn('alt_id', playersToAdd)
+        .whereIn('player_id', alt_ids)
+        .orWhereIn('alt_id', alt_ids)
         .delete();
       log.debug({ res }, 'deleted player_alt');
 
@@ -49,31 +41,13 @@ export default async (
               return { player_id: player_id, alt_id };
             })
         )
-        .into('player_alt');
+        .into('player_alt')
+        .returning('*');
       log.debug({ playersToAdd, rows });
-
-      log.info('Promoted main');
       return rows;
     });
   } catch (e) {
     log.error(e);
   }
   return [];
-};
-
-/**
- * Returns a list of player ids that are alts of the given main id
- * we will use this to reallocate as boxes of the new main.
- *
- * @param main_id
- * @returns
- */
-const getDelegatedAlts = async (main_id: string): Promise<Player[]> => {
-  const knex = await getConnection();
-  const delegatedBoxes = await knex()
-    .select('player_id AS id')
-    .from('player_alt')
-    .orWhere('alt_id', main_id);
-
-  return delegatedBoxes;
 };
