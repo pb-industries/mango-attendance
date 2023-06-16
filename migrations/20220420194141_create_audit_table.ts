cba1f7ab4d1fa065357ed84dd18945a983cb8b3f
Alexander Sims <alexander.sims92@gmail.com>
2023-06-16 07:20:17 +0100

    Add audit table

---
+++ migrations/20220420194141_create_audit_table.ts
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async function (knex) {
+    return knex.schema.createTable('audit', (table) => {
+      table.increments('id').primary();
+      table.integer('user_id').notNullable();
+      table.integer('from_player_id').nullable();
+      table.integer('to_player_id').nullable();
+      table.integer('audit_type');
+
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+      table
+        .foreign('user_id')
+        .references('user.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      table
+        .foreign('target_player_id')
+        .references('player.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
+  },
+  down: async function (knex) {
+    return knex.schema.dropTable('audit');
+  },
+};
